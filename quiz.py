4256a0351dbef7a1a30b77d63bc4fef780245d1f
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-07-08 13:56:14 -0300

    define a lógica para seleção de perguntas conforme o idioma selecionado pelo usuário e inicia a lista de perguntas em português

--- quiz.py
+++ quiz.py
@@ -9,4 +9,25 @@ print("Starting...")
 language_user = input("Qual idioma de sua preferência?/Which language do you prefer?(pt-br/en)\n")
 
 if language_user == "pt-br":
-    
+    perguntas = perguntas_pt
+elif language_user == "en":
+    perguntas = perguntas_en
+else:
+    perguntas = perguntas_pt
+
+
+perguntas_pt = [
+    {
+        "Pergunta": "Qual a capital do Brasil?",
+        "Opções": {
+            "A": "Rio de Janeiro",
+            "B": "Teresina",
+            "C": "Brasília"
+            "D": "Manaus"
+        },
+        "Resposta": "C"
+    },
+    {
+        
+    }
+]     
