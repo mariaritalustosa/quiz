b5a99f8b2650f9dee0644266693a01b746dcf3fb
mariaritalustosa <mritalustosa.ad@gmail.com>
2025-07-08 13:50:18 -0300

    adiciona seleção de idioma com input do usuário

--- quiz.py
+++ quiz.py
@@ -5,3 +5,8 @@ if answer_user != "y":
     quit()
 
 print("Starting...")
+
+language_user = input("Qual idioma de sua preferência?/Which language do you prefer?(pt-br/en)\n")
+
+if language_user == "pt-br":
+    
